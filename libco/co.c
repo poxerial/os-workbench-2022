48902a9135580be92537931ef8129b09bd76b1cd
tracer-nju <tracer@nju.edu.cn>
2022-05-12 12:14:41 +0800

    Linux DESKTOP-2L71IOO 5.10.16.3-microsoft-standard-WSL2 #1 SMP Fri Apr 2 22:23:49 UTC 2021 x86_64 x86_64 x86_64 GNU/Linux  12:14:41 up  1:45,  0 users,  load average: 0.04, 0.04, 0.00

--- libco/co.c
+++ libco/co.c
@@ -5,16 +5,17 @@
 
 #define STACK_SIZE 1 << 23
 
-enum co_status{
-  CO_NEW=1,
-  CO_RUNNING,
-  CO_WAITING,
-  CO_DEAD,
+
+
+enum co_status {
+  CO_NEW = 1,
+  CO_HAS_RUN,
 };
 
-struct co{
-  //ensure 16-byte aligned 
-  uint8_t stack[STACK_SIZE]; 
+struct co
+{
+  // ensure 16-byte aligned
+  uint8_t stack[STACK_SIZE];
 
   char *name;
   void (*func)(void *);
@@ -25,21 +26,79 @@ struct co{
   jmp_buf context;
 };
 
-struct co* current;
+struct co *top;
+struct co *current;
+size_t co_num = 0;
+size_t wait_num = 0;
 
-struct co *co_start(const char *name, void (*func)(void *), void *arg) {
-  struct co* c = (struct co*) malloc(sizeof(struct co));
+static inline void stack_switch_call(void *sp, void *entry, uintptr_t arg)
+{
+  asm volatile(
+#if __x86_64__
+      "movq %0, %%rsp; movq %2, %%rdi; jmp *%1"
+      :
+      : "b"((uintptr_t)sp), "d"(entry), "a"(arg)
+      : "memory"
+#else
+      "movl %0, %%esp; movl %2, 4(%0); jmp *%1"
+      :
+      : "b"((uintptr_t)sp - 8), "d"(entry), "a"(arg)
+      : "memory"
+#endif
+  );
+}
+
+void randjmp()
+{
+  int r = rand();
+  struct co *ne = top;
+  for (r %= co_num; r > 0; r--)
+  {
+    ne = ne->next;
+  }
+  
+  current = ne;
+  if (ne->status == CO_NEW)
+  {
+    ne->status = CO_HAS_RUN;
+    stack_switch_call(ne->stack, ne->func, ne->arg);
+  }
+  else
+  {
+    longjmp(ne->context, 0);
+  }
+}
+struct co *co_start(const char *_name, void (*_func)(void *), void *_arg)
+{
+  struct co *c = (struct co *)malloc(sizeof(struct co));
   if (c == NULL)
     return NULL;
-  c->name = name;
-  c->func = func;
-  c->arg = arg;
+  co_num++;
+  c->name = _name;
+  c->func = _func;
+  c->arg = _arg;
   c->status = CO_NEW;
-  
+  if (top)
+    c->next = top;
+  top = c;
+  return c;
 }
 
-void co_wait(struct co *co) {
+void co_wait(struct co *co)
+{
+  wait_num++;
+
+  if (co_num == wait_num)
+  {
+    current = top;
+    top->status = CO_HAS_RUN;
+    stack_switch_call(top->stack, top->func, top->arg);
+  }
 }
 
-void co_yield() {
+void co_yield()
+{
+  setjmp(current->context);
+
+  randjmp();
 }
