8752ba7c0ec2abeeaf4057d6888ceed2a3c33bae
tracer-nju <tracer@nju.edu.cn>
2022-05-15 21:00:52 +0800

    Linux DESKTOP-2L71IOO 5.10.16.3-microsoft-standard-WSL2 #1 SMP Fri Apr 2 22:23:49 UTC 2021 x86_64 x86_64 x86_64 GNU/Linux  21:00:52 up  6:20,  0 users,  load average: 0.03, 0.03, 0.00

--- libco/co.c
+++ libco/co.c
@@ -84,7 +84,7 @@ void entry(void *args)
 {
   struct co *c = (struct co *)args;
 #if __x86_64__
-  asm volatile ("andq 0xfffffffffffffff0, %rsp");
+  asm volatile ("andq 0xffffffffffffffff, %rsp");
 #endif
   c->func(c->arg);
   wait_num--;
