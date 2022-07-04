6a31f817dada04fef5e7bd89962bfd854bb45166
tracer-nju <tracer@nju.edu.cn>
2022-07-04 09:24:16 +0800

    Linux DESKTOP-2L71IOO 5.10.16.3-microsoft-standard-WSL2 #1 SMP Fri Apr 2 22:23:49 UTC 2021 x86_64 x86_64 x86_64 GNU/Linux  09:24:16 up 4 days, 22:04,  0 users,  load average: 0.07, 0.04, 0.21

--- pstree/pstree.c
+++ pstree/pstree.c
@@ -41,7 +41,7 @@ process procs[MAX_PROCS];
 size_t proc_num;
 
 int proc_comp(const void *a, const void *b) {
-  return ((process *)a)->pid < ((process *)b)->pid;
+  return ((process **)a)[0]->pid < ((process **)b)[0]->pid;
 }
 
 void readProcess() {
