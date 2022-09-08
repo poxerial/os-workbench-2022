76c7808838ee2cbeaf5b7774018b5b8301958465
tracer-nju <tracer@nju.edu.cn>
2022-09-08 21:15:56 +0800

    Linux poxerial-B460MAORUSELITE 5.15.0-46-generic #49~20.04.1-Ubuntu SMP Thu Aug 4 19:15:44 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  21:15:56 up  6:52,  5 users,  load average: 0.31, 0.43, 0.44

--- crepl/crepl.c
+++ crepl/crepl.c
@@ -1,5 +1,33 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
+#include <time.h>
+
+#include <unistd.h>
+#include <sys/time.h>
+#include <wait.h>
+#include <dlfcn.h>
+
+#define EXPR 0
+#define FUNC 1
+
+#ifdef __x86_64
+#define COMPILE_COMMAND "gcc -w -fPIC -shared m64 -ldl"
+#else
+#define COMPILE_COMMAND "gcc -w -fPIC -shared m32 -ldl" 
+#endif
+
+
+extern char **environ;
+
+int check(const char *);
+void gen_name(char *);
+void add_func(const char *);
+void eval_expr(const char *);
+int compile(const char *, const char *);
+void* load(const char *name);
+int execute(void *);
+
 
 int main(int argc, char *argv[]) {
   static char line[4096];
@@ -9,6 +37,104 @@ int main(int argc, char *argv[]) {
     if (!fgets(line, sizeof(line), stdin)) {
       break;
     }
-    printf("Got %zu chars.\n", strlen(line)); // ??
+    if (check(line) == EXPR)
+    {
+      eval_expr(line);
+    }
+    else
+    {
+      add_func(line);
+    }
   }
+}
+
+int check(const char *line)
+{
+  int result;
+  if (strstr(line, "int"))
+    result = FUNC;
+  else
+    result = EXPR;
+  return result;
+}
+
+void gen_name(char *name)
+{
+  struct timeval time;
+  gettimeofday(&time, NULL);
+  sprintf(name, "%ld_%ld", time.tv_sec, time.tv_usec);
+}
+
+void add_func(const char *line)
+{
+  char compile_time_str[64];
+  gen_name(compile_time_str);
+  if (compile(line, compile_time_str))
+  {
+    printf("Compile failed.\n");
+  }
+  else 
+  {
+    load(compile_time_str);
+    printf("Func added.\n");
+  }
+}
+
+void eval_expr(const char *line)
+{
+  char *wrapper_content = (char *)malloc(strlen(line) + 128);
+  char name[64];
+  gen_name(name);
+  strcat(name, "wrapper");
+  sprintf(wrapper_content, 
+  "int %s()"
+  "{"
+  "  return %s;"
+  "}",
+  name, line);
+
+  if (compile(wrapper_content, name))
+  {
+    printf("Compile failed!\n");
+  }
+  else
+  {
+    void *handle = load(name);
+    int result = execute(handle);
+    printf("%d\n", result);
+  }
+}
+
+int compile(const char *codes, const char *name)
+{
+  char filename[64];
+  strcpy(filename, name);
+  FILE *source_code = fopen(strcat(filename, ".c"), "w");
+  fprintf(source_code, "%s", codes);
+  fclose(source_code);
+
+  char compile_commands[256];
+  sprintf(compile_commands, COMPILE_COMMAND" -o %s %s", name, filename);  
+  char *args[] = {compile_commands, NULL};
+
+  int pid = fork();
+  if (pid == 0)
+  {
+    int error_code = execve("gcc", args, environ);
+    exit(-1);
+  }
+  int wait_status;
+  wait(&wait_status);
+  return wait_status; 
+}
+
+void* load(const char *name)
+{
+  return dlopen(name, RTLD_LAZY);
+}
+
+int execute(void *handle)
+{
+  int (*func)() = (int (*)())handle;
+  return func(); 
 }
