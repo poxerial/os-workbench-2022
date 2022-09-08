ce75ce1a7ee6613d291f82ca6e72e5ba3dcfd33a
tracer-nju <tracer@nju.edu.cn>
2022-09-08 21:30:19 +0800

    Linux poxerial-B460MAORUSELITE 5.15.0-46-generic #49~20.04.1-Ubuntu SMP Thu Aug 4 19:15:44 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  21:30:19 up  7:07,  5 users,  load average: 0.64, 0.55, 0.46

--- crepl/crepl.c
+++ crepl/crepl.c
@@ -12,9 +12,11 @@
 #define FUNC 1
 
 #ifdef __x86_64
-#define COMPILE_COMMAND "gcc -Wno-unused-result -Wno-unused-value -Wno-unused-variable -fPIC -shared -m64"
+#define COMPILE_COMMAND "gcc -fPIC -shared -m64 -O1 -std=gnu11 -ggdb" \
+" -Wall -Werror -Wno-unused-result -Wno-unused-value -Wno-unused-variable"
 #else
-#define COMPILE_COMMAND "gcc -Wno-unused-result -Wno-unused-value -Wno-unused-variable -fPIC -shared -m32" 
+#define COMPILE_COMMAND "gcc -fPIC -shared -m64 -O1 -std=gnu11 -ggdb" \
+" -Wall -Werror -Wno-unused-result -Wno-unused-value -Wno-unused-variable" 
 #endif
 
 
