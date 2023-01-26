170b5fffa555b436fae7a555cf6d4d43250720c8
tracer-nju <tracer@nju.edu.cn>
2023-01-26 09:13:27 +0800

    Linux DESKTOP-2L71IOO 5.15.79.1-microsoft-standard-WSL2 #1 SMP Wed Nov 23 01:01:46 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  09:13:27 up 33 min,  0 users,  load average: 0.01, 0.00, 0.00

--- sperf/sperf.c
+++ sperf/sperf.c
@@ -1,13 +1,26 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
+#include <assert.h>
+
+
 
 int main(int argc, char *argv[]) {
+
+  // int pipedes[2];
+  // assert(pipe(pipedes) == 0);
+
+  // int pid = fork();
+
+  // if (pid == 0) {
+
+  // }
+
   char *exec_argv[] = { "strace", "ls", NULL, };
   char *exec_envp[] = { "PATH=/bin", NULL, };
-  execve("strace",          exec_argv, exec_envp);
-  execve("/bin/strace",     exec_argv, exec_envp);
-  execve("/usr/bin/strace", exec_argv, exec_envp);
+  execve("strace",          argv, exec_envp);
+  execve("/bin/strace",     argv, exec_envp);
+  execve("/usr/bin/strace", argv, exec_envp);
   perror(argv[0]);
   exit(EXIT_FAILURE);
 }
