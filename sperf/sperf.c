1c2e5f0cfec31887a64aacc1c9bfeac6222ef9df
tracer-nju <tracer@nju.edu.cn>
2023-01-26 09:37:03 +0800

    Linux DESKTOP-2L71IOO 5.15.79.1-microsoft-standard-WSL2 #1 SMP Wed Nov 23 01:01:46 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  09:37:03 up 56 min,  0 users,  load average: 0.00, 0.00, 0.00

--- sperf/sperf.c
+++ sperf/sperf.c
@@ -20,12 +20,13 @@ int main(int argc, char *argv[]) {
   char *exec_envp[] = { "PATH=/bin", NULL, };
 
 
-  char **exec_argv= (char**)alloca((argc + 1) * sizeof(char *));
+  char **exec_argv= (char**)alloca((argc + 2) * sizeof(char *));
   exec_argv[0] = "strace";
   exec_argv[1] = "-r";
   for (int i = 1; i < argc; i++) {
     exec_argv[i + 1] = argv[i];
   }
+  exec_argv[argc + 1] = NULL;
 
   execve("strace",          exec_argv, exec_envp);
   execve("/bin/strace",     exec_argv, exec_envp);
