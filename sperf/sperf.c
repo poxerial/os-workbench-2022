832039414585b551abce9998525320b4680d3228
tracer-nju <tracer@nju.edu.cn>
2023-01-26 10:50:17 +0800

    Linux DESKTOP-2L71IOO 5.15.79.1-microsoft-standard-WSL2 #1 SMP Wed Nov 23 01:01:46 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  10:50:17 up  2:10,  0 users,  load average: 0.05, 0.02, 0.00

--- sperf/sperf.c
+++ sperf/sperf.c
@@ -1,43 +1,41 @@
-#include <alloca.h>
 #include <assert.h>
+#include <bits/types/time_t.h>
+#include <regex.h>
 #include <stdio.h>
 #include <stdlib.h>
+
 #include <unistd.h>
 
 #define BUFFER_SIZE 128
 
 int main(int argc, char *argv[]) {
 
-  char *exec_envp[] = {
-      "PATH=/bin",
-      NULL,
-  };
-  char **exec_argv = (char **)alloca((argc + 2) * sizeof(char *));
-  exec_argv[0] = "strace";
-  exec_argv[1] = "-T";
-  for (int i = 1; i < argc; i++) {
-    exec_argv[i + 1] = argv[i];
-  }
-  exec_argv[argc + 1] = NULL;
-
-  execve("strace", exec_argv, exec_envp);
-  execve("/bin/strace", exec_argv, exec_envp);
-  execve("/usr/bin/strace", exec_argv, exec_envp);
-  perror(argv[0]);
-  exit(EXIT_FAILURE);
-
   int pipedes[2];
   assert(pipe(pipedes) == 0);
   int pid = fork();
   if (pid == 0) {
     dup2(STDERR_FILENO, pipedes[1]);
+
+    char *exec_envp[] = {
+        "PATH=/bin",
+        NULL,
+    };
+    char **exec_argv = (char **)alloca((argc + 2) * sizeof(char *));
+    exec_argv[0] = "strace";
+    exec_argv[1] = "-T";
+    for (int i = 1; i < argc; i++) {
+      exec_argv[i + 1] = argv[i];
+    }
+    exec_argv[argc + 1] = NULL;
+
     execve("strace", exec_argv, exec_envp);
     execve("/bin/strace", exec_argv, exec_envp);
     execve("/usr/bin/strace", exec_argv, exec_envp);
     perror(argv[0]);
     exit(EXIT_FAILURE);
   } else {
-    char buffer[BUFFER_SIZE] = {0}; 
+    char buffer[BUFFER_SIZE] = {0};
+
     while (read(pipedes[0], buffer, BUFFER_SIZE) != 0) {
       printf("%s", buffer);
     }
