3a123c421c684ddf3ac4df6786c9427d60d117fb
tracer-nju <tracer@nju.edu.cn>
2023-01-26 18:13:57 +0800

    Linux DESKTOP-2L71IOO 5.15.79.1-microsoft-standard-WSL2 #1 SMP Wed Nov 23 01:01:46 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  18:13:57 up  9:33,  0 users,  load average: 0.00, 0.03, 0.00

--- sperf/sperf.c
+++ sperf/sperf.c
@@ -65,7 +65,7 @@ void *wait_and_close_pipe(void *args) {
   int pid = ((int *)args)[0], des = ((int *)args)[1];
   waitpid(pid, NULL, 0);
   close(des);
-  printf("close pipe %d\n", des);
+  printf("%d returned and close pipe %d\n", pid, des);
   return NULL;
 }
 
